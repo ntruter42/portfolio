b5472ed12ba7f4f10795ffb92a22726fdc47cd16
ntruter42 <ntruter42@gmail.com>
2023-03-30 13:29:02 +0200

    Add node-js-placeholder, Update index.html, styles.css, scripts.js - separated front-end and backe-end projects, moved github repo link to right side of project screenshots

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,11 @@
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
 	var nav = document.querySelector("nav");
 	nav.classList.toggle("scrolled", window.scrollY > 0);
 });
+
+window.onload = function () {
+	var links = document.getElementsByTagName('a');
+	for (var i = 0; i < links.length; i++) {
+		links[i].setAttribute('target', '_blank');
+	}
+};
